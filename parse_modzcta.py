f60fb10a8449cdfcb0fc2d6068a25a41610c07e5
dquach217 <dq6180@rit.edu>
2020-09-26 17:54:42 -0400

    covid-19 data

--- parse_modzcta.py
+++ parse_modzcta.py
@@ -1,12 +1,14 @@
 import os
 import json
 import googlemaps
-
+import random
 gmaps = googlemaps.Client(key=os.getenv('GOOGLE_API_KEY'))
 
 
 def read_csv_modzcta():
     modzcta = {}
+    lat = list()
+    lng = list()
     with open('dataset/COVID-19/data_by_modzcta.csv', 'r') as data:
         data.readline().split(',')
         lines = data.readlines()
@@ -17,16 +19,31 @@ def read_csv_modzcta():
             line = line.strip().split(",")
 
             geocode_result = gmaps.geocode(str(line[0]))
-
+            print(geocode_result)
             data["COVID_CASE_COUNT"], data["COVID_CASE_RATE"], data["COVID_DEATH_COUNT"], data["COVID_DEATH_RATE"], data["PERCENT_POSITIVE"], data["TOTAL_COVID_TESTS"] = int(line[3]), float(line[4]), int(line[6]), float(line[7]), float(line[8]), int(line[9])
             location["LAT"], location["LNG"]= geocode_result[0]['geometry']["location"]["lat"],  geocode_result[0]['geometry']["location"]["lng"]
             data["LOCATION"] = location
             modzcta[line[1]] = data
+            count = int(data["COVID_CASE_COUNT"])
+            small_lat = float(geocode_result[0]['geometry']["viewport"]["northeast"]["lat"])
+            large_lat = float(geocode_result[0]['geometry']["viewport"]["southwest"]["lat"])
+            small_lng = float(geocode_result[0]['geometry']["viewport"]["northeast"]["lng"])
+            large_lng = float(geocode_result[0]['geometry']["viewport"]["southwest"]["lng"])
+            for i in range(count):
+                lat.append(random.uniform(small_lat, large_lat))
+                lng.append(random.uniform(small_lng, large_lng))
 
     with open('dataset/COVID-19/parse_covid_data.json', 'w') as outfile:
         json.dump(modzcta, outfile)
         outfile.close()
-
+    with open('dataset/latitude.txt', 'w') as f:
+        for l in lat:
+            f.write("%s\n" % l)
+        f.close()
+    with open('dataset/longitude.txt', 'w') as f:
+        for l in lng:
+            f.write("%s\n" % l)
+        f.close()
 
 def read_parse_covid_data():
     lat = []
@@ -48,5 +65,5 @@ def read_parse_covid_data():
 
 
 if __name__ == "__main__":
-    # read_csv_modzcta()
-    read_parse_covid_data()
+    read_csv_modzcta()
+    # read_parse_covid_data()
